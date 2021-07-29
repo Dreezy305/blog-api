3ae3d07801f2f83a95204dc038150f6d568c61a1
Dreezy305 <bankoleidris@gmail.com>
2021-07-29 08:05:50 +0000

    working on config

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const config = require("./app/config");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.set("port", config.port);
+
+app.listen(app.get("port"), () => {
+  console.log(`listening on port ${app.get("port)}....`);
 });
 
 app.get("/", (req, res) => {
