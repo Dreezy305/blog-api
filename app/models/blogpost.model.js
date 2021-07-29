011d182b0b27bb7648c54060dabb9d3a2ca1f62a
Dreezy305 <bankoleidris@gmail.com>
2021-07-29 08:21:58 +0000

    working on blogpost schema

--- app/models/blogpost.model.js
+++ app/models/blogpost.model.js
@@ -1,1 +1,32 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+// creating blop post schema
+const blogPostSchema = new Schema({
+  id: ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  date: { type: Date, default: Date.now },
+  description: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  url: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
