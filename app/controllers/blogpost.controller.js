cdc0ea7d774d81a0b49a5fb42b41476b37d83508
Dreezy305 <bankoleidris@gmail.com>
2021-07-29 08:48:24 +0000

    working on blogpost controller

--- app/controllers/blogpost.controller.js
+++ app/controllers/blogpost.controller.js
@@ -2,3 +2,14 @@
 const blogPostModel = require("../models/blogpost.model");
 
 // create new blog post
+exports.publishPost = (req, res) => {
+  const newBlogPost = new blogPostModel(req.body);
+  newBlogPost.save((error, blogPostModel) => {
+    if (error) {
+      res.status(400).json({
+        msg: "",
+        error,
+      });
+    }
+  });
+};
