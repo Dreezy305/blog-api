2778326f3fd7c5f2249c906c31b66d310a54cadc
Dreezy305 <bankoleidris@gmail.com>
2021-07-30 11:41:45 +0000

    comment model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const config = require("./app/config");
 const blogs = require("./blogs");
+const comments = require("./app/models/comment.model");
 
 // const router = require("./app/router");
 // router(app);
