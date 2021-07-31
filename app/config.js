7f0f33357f3736a15ea2a1ba8921834bc4c975a1
Dreezy305 <bankoleidris@gmail.com>
2021-07-31 14:44:23 +0000

    pushing blogs to db

--- app/config.js
+++ app/config.js
@@ -1,8 +1,6 @@
 const config = {
   port: process.env.PORT || 5000,
   db: process.env.DATABASE_URL,
-  // test_port: 4242,
-  // test_db: "mongodb://localhost/myblog_test",
 };
 
 module.exports = config;
